ffc3591467e5645f44e0083afd7ff01d6cc7a71f
Susanna Braun <su.braun@gmx.de>
2021-12-20 14:37:10 +0100

    initial load

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-app';
+  picture = 'https://cdn.pixabay.com/photo/2016/11/14/09/14/cat-1822979_1280.jpg'
+}
